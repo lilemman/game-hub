9138207fff967db4a90d41f5c63a3303eeeae1f7
Emmanuel Atuahene <62813726+lilemman@users.noreply.github.com>
2024-08-27 01:14:11 -0400

    displaying critic score

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -31,7 +31,7 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack margin={1}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color="green.500" />
+        <Icon as={iconMap[platform.slug]} color="grey.500" />
       ))}
     </HStack>
   );
